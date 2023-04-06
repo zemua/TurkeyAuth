924cebedf429f0584a094754a4a3d9a337585fb0
Miguel Rietz <miguel.rietz@netcentric.biz>
2023-04-06 19:54:26 +0200

    initial commit

---
+++ TurkeyAuth/src/main/java/devs/mrp/springturkey/TurkeyAuthApplication.java
@@ -0,0 +1,13 @@
+package devs.mrp.springturkey;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TurkeyAuthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TurkeyAuthApplication.class, args);
+	}
+
+}
---
+++ TurkeyAuth/src/test/java/devs/mrp/springturkey/TurkeyAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package devs.mrp.springturkey;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TurkeyAuthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
