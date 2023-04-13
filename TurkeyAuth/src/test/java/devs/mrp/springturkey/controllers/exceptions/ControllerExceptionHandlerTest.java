5aae553472b4370c11b729e007adaddd9a15577c
Miguel Rietz <miguel.rietz@netcentric.biz>
2023-04-14 01:59:35 +0200

    test exceptions

---
+++ TurkeyAuth/src/test/java/devs/mrp/springturkey/controllers/exceptions/ControllerExceptionHandlerTest.java
@@ -0,0 +1,19 @@
+package devs.mrp.springturkey.controllers.exceptions;
+
+import static org.junit.jupiter.api.Assertions.fail;
+
+import org.junit.jupiter.api.Test;
+
+class ControllerExceptionHandlerTest {
+
+	@Test
+	void testHandleKeycloakClientUnauthorizedException() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testHandleClientRequestException() {
+		fail("Not yet implemented");
+	}
+
+}
