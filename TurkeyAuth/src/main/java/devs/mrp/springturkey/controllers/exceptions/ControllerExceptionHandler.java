9f5fd17bd93d0ba0aef360d0e77e22dd92a15c70
Miguel Rietz <miguel.rietz@netcentric.biz>
2023-04-14 19:47:02 +0200

    test controller exception handling

--- TurkeyAuth/src/main/java/devs/mrp/springturkey/controllers/exceptions/ControllerExceptionHandler.java
+++ TurkeyAuth/src/main/java/devs/mrp/springturkey/controllers/exceptions/ControllerExceptionHandler.java
@@ -1,11 +1,13 @@
 package devs.mrp.springturkey.controllers.exceptions;
 
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import devs.mrp.springturkey.exceptions.ClientRequestException;
 import devs.mrp.springturkey.exceptions.KeycloakClientUnauthorizedException;
+import devs.mrp.springturkey.exceptions.TokenRetrievalException;
 import lombok.extern.slf4j.Slf4j;
 
 @RestControllerAdvice
@@ -22,6 +24,12 @@ public class ControllerExceptionHandler {
 	public ResponseEntity<?> handleClientRequestException(ClientRequestException ex) {
 		log.error("Handling exception:", ex);
 		return ResponseEntity.status(ex.getResponse()).build();
+	}
+
+	@ExceptionHandler(TokenRetrievalException.class)
+	public ResponseEntity<?> handleTokenRetrievalException(TokenRetrievalException ex) {
+		log.error("Handling expceiont: ", ex);
+		return ResponseEntity.status(HttpStatusCode.valueOf(523)).build();
 	}
 
 }
--- TurkeyAuth/src/test/java/devs/mrp/springturkey/controllers/exceptions/ControllerExceptionHandlerTest.java
+++ TurkeyAuth/src/test/java/devs/mrp/springturkey/controllers/exceptions/ControllerExceptionHandlerTest.java
@@ -1,19 +1,51 @@
 package devs.mrp.springturkey.controllers.exceptions;
 
-import static org.junit.jupiter.api.Assertions.fail;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+
+import devs.mrp.springturkey.exceptions.ClientRequestException;
+import devs.mrp.springturkey.exceptions.KeycloakClientUnauthorizedException;
+import devs.mrp.springturkey.exceptions.TokenRetrievalException;
 
 class ControllerExceptionHandlerTest {
 
+	private ControllerExceptionHandler controllerExceptionHandler;
+
+	@BeforeEach
+	void setup() {
+		controllerExceptionHandler = new ControllerExceptionHandler();
+	}
+
 	@Test
 	void testHandleKeycloakClientUnauthorizedException() {
-		fail("Not yet implemented");
+		KeycloakClientUnauthorizedException ex = new KeycloakClientUnauthorizedException();
+		ResponseEntity<?> response = controllerExceptionHandler.handleKeycloakClientUnauthorizedException(ex);
+		assertEquals(500, response.getStatusCode().value());
 	}
 
 	@Test
 	void testHandleClientRequestException() {
-		fail("Not yet implemented");
+		ClientRequestException ex = new ClientRequestException();
+		ResponseEntity<?> response = controllerExceptionHandler.handleClientRequestException(ex);
+		assertEquals(400, response.getStatusCode().value());
+	}
+
+	@Test
+	void testHandleClientRequestExceptionSpecificCode() {
+		ClientRequestException ex = new ClientRequestException(HttpStatusCode.valueOf(409));
+		ResponseEntity<?> response = controllerExceptionHandler.handleClientRequestException(ex);
+		assertEquals(409, response.getStatusCode().value());
+	}
+
+	@Test
+	void testHandleTokenRetrievalException() {
+		TokenRetrievalException ex = new TokenRetrievalException();
+		ResponseEntity<?> response = controllerExceptionHandler.handleTokenRetrievalException(ex);
+		assertEquals(523, response.getStatusCode().value());
 	}
 
 }
